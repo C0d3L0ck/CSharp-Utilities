de942eeb182e02be298135518d8ecece8eac89f3
CodeLock <105467967+C0d3L0ck@users.noreply.github.com>
2025-02-05 14:11:27 -0400

    Add files via upload
    PDF o EXCEL (XLS) Process Render (WINUI3)

---
+++ RenderizeFormatMethod.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+
+namespace RetentionDesign
+{
+    class RenderizeFormatMethod
+    {
+        private async void RenderizeInWeb(byte[] bytes, string format)
+        {
+            string base64 = Convert.ToBase64String(bytes);
+
+            string filePath = Path.Combine(Path.GetTempPath(), $"temp.{format.ToLower()}"); // PDF OR XLS 
+
+            File.WriteAllBytes(filePath, bytes);
+
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo(filePath)
+                    {
+                        UseShellExecute = true
+                    };
+                    Process.Start(startInfo);
+                }
+                catch (Exception ex)
+                {
+                    ContentDialog content = new()
+                    {
+                       // XamlRoot = this.XamlRoot,
+                        Title = "Error",
+                        Content = "No se pudo abrir el archivo. Error: " + ex.Message,
+                        CloseButtonText = "Cerrar"
+                    };
+                    content.ShowAsync();
+                }
+            }
+            else
+            {
+                ContentDialog content = new()
+                {
+                   // XamlRoot = this.XamlRoot,
+                    Title = "Error",
+                    Content = "El archivo no existe: " + filePath,
+                    CloseButtonText = "Cerrar"
+                };
+                content.ShowAsync();
+            }
+        }
+    }
+}
